7d6f684dd0b9377240dc2992ea5f7226fff7fcc4
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-04 14:05:44 +0200

    Add: structured_output.py manipulate function print and make first script in python

---
+++ python_fundamentals/hello_world/README.md
@@ -0,0 +1,3 @@
+Python - Environment & First Programs
+
+Structured_output.py is an try to print an exact example of an render
---
+++ python_fundamentals/hello_world/structured_output.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+language = "Python"
+version = 3
+pi_value = 3.140824570
+pi_approx = f"{pi_value:.2f}"
+computation_valid = 1 == 1
+
+print(f"Language: {language}")
+print(f"Version: {version}")
+print(f"Pi approx: {pi_approx}")
+print(f"Computation valid: {computation_valid}")
