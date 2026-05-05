cfebd46f3680f69889936110194f628febb19ba8
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-05 09:59:31 +0200

    Add: script return if the last digit of random number is <||> 0

--- python_fundamentals/control_flow/last_digit.py
+++ python_fundamentals/control_flow/last_digit.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+
+number = __import__('random').randint(-10000, 10000)
+
+if number < 0:
+    last_digit = -(abs(number) % 10)
+else:
+    last_digit = number % 10
+
+if last_digit == 0:
+    print(f"Last digit of {number} is {last_digit} and is zero")
+elif last_digit > 5:
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
+else:
+    print(f"Last digit of {number} is "
+          f"{last_digit} and is less than 6 and not 0")
