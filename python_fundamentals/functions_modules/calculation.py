5e51ae01f454d37bf55d0f87cf9d9dc5900858ba
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-05 18:05:50 +0200

    Feat: using function delivred by another file

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import div
+from calculator_1 import mul
+
+print(add(10, 5))
+print(sub(5, 10))
+print(mul(5, 10))
+print(div(5, 10))
