298dba63df95957a93cad814cab4f64da3877aa4
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-05 18:02:04 +0200

    Add: function uppercase return new str

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,1 +1,10 @@
 #!/usr/bin/env python3
+def uppercase(str):
+    new_str = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            new_char = chr(ord(char) - 32)
+        else:
+            new_char = char
+        new_str += new_char
+    return new_str
