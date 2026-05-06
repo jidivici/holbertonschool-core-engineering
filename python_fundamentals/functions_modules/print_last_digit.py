efcf27bd9a5db22d5009ecf2ed3e6d9f1302820a
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-06 11:43:23 +0200

    Fix: using strict format

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 
 def print_last_digit(number):
-    if number < 0:
-        new_number = -abs(number) % 10
-    else:
-        new_number = abs(number) % 10
-    print(new_number)
+    new_number = abs(number) % 10
+    print("{}".format(new_number))
     return new_number
