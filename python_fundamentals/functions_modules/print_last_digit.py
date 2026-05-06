0a753fff7d2089f2005ffe58aa45b51128327ae6
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-06 19:28:17 +0200

    Fix: dont use abs

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
 def print_last_digit(number):
-    new_number = abs(number) % 10
-    return new_number
+    last = number % 10
+    return last if number >= 0 else (10 - last if last != 0 else 0)
+
