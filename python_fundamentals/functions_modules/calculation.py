c5eb6d131b7ec184217206084e9086b5b0c15b5e
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-06 19:48:37 +0200

    Fix: The program must not execute when imported

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -4,7 +4,8 @@ from calculator_1 import sub
 from calculator_1 import div
 from calculator_1 import mul
 
-print("{}".format(add(10, 5)))
-print("{}".format(sub(10, 5)))
-print("{}".format(div(10, 5)))
-print("{}".format(mul(10, 5)))
+if __name__ == "__main__":
+    print("{}".format(add(10, 5)))
+    print("{}".format(sub(10, 5)))
+    print("{}".format(div(10, 5)))
+    print("{}".format(mul(10, 5)))
