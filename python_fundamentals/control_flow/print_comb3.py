da918dad1f5a60a2e3ca3717abe316826793f690
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-06 11:16:31 +0200

    Fix: modify range of loop + coding style

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -2,4 +2,5 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{}{}".format(i, j), end=", " if not (i == 8 and j == 9) else "\n")
+        print("{}{}".format(i, j), end=", "
+              if not (i == 8 and j == 9) else "\n")
