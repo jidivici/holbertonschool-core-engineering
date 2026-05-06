43b0ec9c74d0b56143304cea671f85e1a579aba9
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-06 11:07:39 +0200

    Fix: using stric format

--- python_fundamentals/control_flow/print_comb3.py
+++ python_fundamentals/control_flow/print_comb3.py
@@ -2,4 +2,4 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print(f"{i}{j}", end=", " if not (i == 8 and j == 9) else "\n")
+        print("{}{}".format(i, j), end=", " if not (i == 8 and j == 9) else "\n")
