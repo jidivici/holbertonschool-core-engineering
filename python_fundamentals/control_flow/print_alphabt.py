53269f7c4bfef45fe9ed5aea2d8adfecd25ba9dd
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-06 11:00:24 +0200

    Fix: use basic loop and handle strict format

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,11 +1,4 @@
 #!/usr/bin/env python3
-
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-i = 0
-
-while i < len(alphabet):
-    if alphabet[i] != 'e' and alphabet[i] != 'q':
-        print(alphabet[i], end="")
-    i += 1
-
-print()
+for c in "abcdefghijklmnopqrstuvwxyz":
+    if c != 'e' and c != 'q':
+        print("{}".format(c), end="")
