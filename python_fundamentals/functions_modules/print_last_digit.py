683cb2bc0a70ce6654cbe6cc1fd49995e7b80ab2
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-06 11:39:57 +0200

    Fix: handling negative number

--- python_fundamentals/functions_modules/print_last_digit.py
+++ python_fundamentals/functions_modules/print_last_digit.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 def print_last_digit(number):
-    new_number = abs(number) % 10
+    if number < 0:
+        new_number = -abs(number) % 10
+    else:
+        new_number = abs(number) % 10
     print(new_number)
     return new_number
