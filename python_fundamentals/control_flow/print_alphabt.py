dfcd25d57bf644695c547c00ef181a38f49ec8ca
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-06 10:45:37 +0200

    Fix: without use of chr

--- python_fundamentals/control_flow/print_alphabt.py
+++ python_fundamentals/control_flow/print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
 
-for i in range(97, 123):
-    letter = chr(i)
-    if letter not in ('e', 'q'):
-        print(letter, end="")
-print()
+alphabet = "abcdfghijklmnoprstuvwxyz"
+print(alphabet)
