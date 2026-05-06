9a20bc72387edb0fb36c4e002cc5608f6d58b8cd
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-06 11:35:58 +0200

    Fix: print direct the result

--- python_fundamentals/functions_modules/uppercase.py
+++ python_fundamentals/functions_modules/uppercase.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    lower = "abcdefghijklmnopqrstuvwxyz"
-    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    new_str = ""
     for char in str:
-        i = 0
-        found = False
-        while i < 26:
-            if char == lower[i]:
-                print("{}".format(upper[i]), end="")
-                found = True
-                break
-            i += 1
-        if not found:
-            print("{}".format(char), end="")
-    print()
+        if 'a' <= char <= 'z':
+            new_char = chr(ord(char) - 32)
+        else:
+            new_char = char
+        new_str += new_char
+    print ("{}".format(new_str))
