434d6a3a245e9a15876b4c896195b6f6ae14e009
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-07 14:58:12 +0200

    Add: function permit us to add two tuple together with the form tot = (a1 + b1, a2 + b2)

--- python_fundamentals/core_data_structures/add_tuple.py
+++ python_fundamentals/core_data_structures/add_tuple.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+def add_tuple(tuple_a = (), tuple_b = ()):
+    a_first = tuple_a[0] if len(tuple_a) > 0 else 0
+    a_second = tuple_a[1] if len(tuple_a) > 1 else 0
+
+    b_first = tuple_b[0] if len(tuple_b) > 0 else 0
+    b_second = tuple_b[1] if len(tuple_b) > 1 else 0
+
+    tot = (a_first + b_first, a_second + b_second)
+    return tot
