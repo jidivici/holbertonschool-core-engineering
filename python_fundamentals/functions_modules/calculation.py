e8d57095848e1d4c9fce96343a0b0052646084a1
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-07 13:46:57 +0200

    Fix: simple cot for exclusion only main

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from calculator_1 import add, sub, div, mul
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     a = 10
     b = 5
     print("{} + {} = {}".format(a, b, add(a, b)))
