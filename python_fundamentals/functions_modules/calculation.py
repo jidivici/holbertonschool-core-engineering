86878b99c9ddbbdd0c43203174c0e92380b29983
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-07 13:44:26 +0200

    Fix: import all in one line

--- python_fundamentals/functions_modules/calculation.py
+++ python_fundamentals/functions_modules/calculation.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import div
-from calculator_1 import mul
-
+from calculator_1 import add, sub, div, mul
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{}".format(add(a, b)))
-    print("{}".format(sub(a, b)))
-    print("{}".format(mul(a, b)))
-    print("{}".format(div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
