9bd17223f4b4292aafae078a1b0fa4fd7e6891d3
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-08 10:53:00 +0200

    Fix: handle position in loop and print " ," when is not the last element

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in range(len(row)):
             end_char = ", " if element != len(row) - 1 else ""
-            print(row[i], end=end_char)
+            print("{}".format(row[element]), end=end_char)
         print()
