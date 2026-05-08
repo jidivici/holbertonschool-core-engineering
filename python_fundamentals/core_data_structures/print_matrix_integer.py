bc0f950b0fa4feaee09a1386c70a912f19ddc2d8
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-08 11:05:57 +0200

    Fix: mistake have not to print " ," but handle with tyope :d

--- python_fundamentals/core_data_structures/print_matrix_integer.py
+++ python_fundamentals/core_data_structures/print_matrix_integer.py
@@ -3,6 +3,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in range(len(row)):
-            end_char = ", " if element != len(row) - 1 else ""
-            print("{}".format(row[element]), end=end_char)
+            print("{:d}".format(row[element]), end=" " if element != len(row) - 1 else "")
         print()
