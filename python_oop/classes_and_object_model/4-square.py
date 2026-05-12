54fe83e33434e864717b7e31972e028342ff3e93
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-12 14:02:05 +0200

    Add: class init square object + methods area return area of instance/object square + getter and setter def

---
+++ python_oop/classes_and_object_model/4-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+class Square:
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
