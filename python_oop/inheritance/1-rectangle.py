ab6bc814cbb3c84ebb1de292340bae4a0d937662
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-14 16:45:24 +0200

    Add: Class base geometry

---
+++ python_oop/inheritance/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Module that defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometric shapes.
+    Provides common validation and interface methods.
+    """
+
+
+    def area(self):
+        """
+        Raises an exception because area is not implemented.
+
+        Raises:
+            Exception: area is not implemented
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates that value is a positive integer.
+
+        Args:
+            name (str): The name of the value
+            value (int): The value to validate
+
+        Raises:
+            TypeError: if value is not an integer
+            ValueError: if value is less than or equal to 0
+        """
+        if isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
