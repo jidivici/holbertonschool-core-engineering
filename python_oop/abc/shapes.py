0af563e1da14209225cfb88b5173aa7a81cecfbb
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-15 17:14:19 +0200

    Rollback: test

--- python_oop/abc/shapes.py
+++ python_oop/abc/shapes.py
@@ -23,15 +23,12 @@ class Circle(Shape):
     """Defines a circle."""
 
     def __init__(self, radius):
-        """Initialize a circle."""
         self.radius = radius
 
     def area(self):
-        """Return the area of the circle."""
         return math.pi * (self.radius ** 2)
 
     def perimeter(self):
-        """Return the perimeter of the circle."""
         return 2 * math.pi * self.radius
 
 
@@ -39,28 +36,17 @@ class Rectangle(Shape):
     """Defines a rectangle."""
 
     def __init__(self, width, height):
-        """Initialize a rectangle."""
         self.width = width
         self.height = height
 
     def area(self):
-        """Return the area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
-        """Return the perimeter of the rectangle."""
         return 2 * (self.width + self.height)
 
 
 def shape_info(shape):
-    """Print the area and perimeter of a shape."""
+    """Prints area and perimeter of a shape."""
     print("Area: {}".format(shape.area()))
     print("Perimeter: {}".format(shape.perimeter()))
-
-
-if __name__ == "__main__":
-    circle = Circle(radius=5)
-    rectangle = Rectangle(width=4, height=7)
-
-    shape_info(circle)
-    shape_info(rectangle)
