02906ce01a09bed0328817b981fdbe29ed2fded3
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-15 15:33:40 +0200

    Add: class rectangle initialise 2 private value

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,39 +1,46 @@
 #!/usr/bin/env python3
-"""
-Module that defines the BaseGeometry class.
-"""
+"""Module that defines the Rectangle class that inherits from BaseGeometry."""
 
 
 class BaseGeometry:
-    """
-    Base class for geometric shapes.
-    Provides common validation and interface methods.
-    """
-
+    """Base class for geometric shapes."""
 
     def area(self):
-        """
-        Raises an exception because area is not implemented.
+        """Raise an exception because area is not implemented.
 
         Raises:
-            Exception: area is not implemented
+            Exception: area() is not implemented.
         """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validates that value is a positive integer.
+        """Validate that value is a positive integer.
 
         Args:
-            name (str): The name of the value
-            value (int): The value to validate
+            name (str): The name of the value.
+            value (int): The value to validate.
 
         Raises:
-            TypeError: if value is not an integer
-            ValueError: if value is less than or equal to 0
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
         """
-        if isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Defines a rectangle, inherits from BaseGeometry."""
+
+    def __init__(self, width, height):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
