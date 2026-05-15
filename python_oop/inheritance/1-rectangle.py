4f7ebbdd97b41ffdb88336602348d9b048e33c1a
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-15 15:41:07 +0200

    fix: import base_geometry.py

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,33 +1,6 @@
 #!/usr/bin/env python3
 """Module that defines the Rectangle class that inherits from BaseGeometry."""
-
-
-class BaseGeometry:
-    """Base class for geometric shapes."""
-
-    def area(self):
-        """Raise an exception because area is not implemented.
-
-        Raises:
-            Exception: area() is not implemented.
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate that value is a positive integer.
-
-        Args:
-            name (str): The name of the value.
-            value (int): The value to validate.
-
-        Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than or equal to 0.
-        """
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+from base_geometry import BaseGeometry
 
 
 class Rectangle(BaseGeometry):
