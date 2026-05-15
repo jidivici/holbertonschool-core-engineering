126a199efae7ec764e28c73b9922bd26e10a6099
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-15 17:28:34 +0200

    fix: use abs at the init to avoid evry problem of -number

--- python_oop/abc/shapes.py
+++ python_oop/abc/shapes.py
@@ -36,8 +36,8 @@ class Rectangle(Shape):
     """Defines a rectangle."""
 
     def __init__(self, width, height):
-        self.width = abs(width)
-        self.height = abs(height)
+        self.width = width
+        self.height = height
 
     def area(self):
         return self.width * self.height
