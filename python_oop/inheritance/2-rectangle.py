70956c80aa973d7e8ae79f2db4032a0d3188d528
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-15 16:32:56 +0200

    Fix: use getter in rectangle

--- python_oop/inheritance/2-rectangle.py
+++ python_oop/inheritance/2-rectangle.py
@@ -18,10 +18,6 @@ class Rectangle(BaseGeometry):
         """Return the area of the rectangle."""
         return self.__width * self.__height
 
-    def width(self):
-        """Return the width of the rectangle."""
-        return self.__width
-
     def __str__(self):
         """Return a string representation of the rectangle."""
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
