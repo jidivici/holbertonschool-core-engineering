c903d66c6e62be19b332ea174ca272c64b900a3f
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-15 18:47:05 +0200

    Fix: use good docstring

--- python_oop/abc/dragon.py
+++ python_oop/abc/dragon.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module that defines Dragon, fly, and swim classes using ."""
 
+
 class SwimMixin:
     """Mixin providing swimming capability to classes."""
 
@@ -8,12 +9,14 @@ class SwimMixin:
         """Make the creature swim."""
         print("The creature swims!")
 
+
 class FlyMixin:
     """Mixin providing flying capability to classes."""
 
     def fly(self):
         """Make the creature fly."""
         print("The creature flies!")
+
 
 class Dragon(SwimMixin, FlyMixin):
     """Dragon class combining swimming and flying abilities."""
