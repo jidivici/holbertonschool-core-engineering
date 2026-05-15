25ebfae470305907984e42267858d567be704ff8
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-15 15:44:53 +0200

    fix: import base_geometry.py

--- python_oop/inheritance/1-rectangle.py
+++ python_oop/inheritance/1-rectangle.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
 """Module that defines the Rectangle class that inherits from BaseGeometry."""
-from base_geometry import BaseGeometry
+
+BaseGeometry = __import__('0-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Defines a rectangle, inherits from BaseGeometry."""
 
     def __init__(self, width, height):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
+        """Initialize a new Rectangle."""
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
