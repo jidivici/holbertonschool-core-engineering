7c3b9ee8c42fa6039cafb99b2cf2383ff8dc8e7e
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-25 15:44:36 +0200

    Add: docstring to write_file module and function

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""Writes text to a file, overwriting any existing content."""
 
 def write_file(filename="", text=""):
+    """Writes text to the given file and returns the number of characters written."""
         with open(filename, mode="w") as f:
             return f.write(text)
