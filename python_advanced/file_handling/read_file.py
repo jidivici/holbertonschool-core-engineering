e085954a780c3de68affd0229be49573912e0cb5
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-25 15:44:31 +0200

    Add: docstring to read_file module and function

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+"""Reads and returns the content of a file."""
 
 def read_file(filename=""):
+    """Opens and returns the full content of the given file."""
     with open(filename, mode="r") as f:
         return f.read()
