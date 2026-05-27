3b3d43ba919c95624d7ad83bfe75bda842448575
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-05-27 16:44:40 +0200

    Add: print UTF-8 content to stdout in read_file

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -3,5 +3,5 @@
 
 def read_file(filename=""):
     """Opens and returns the full content of the given file."""
-    with open(filename, mode="r") as f:
-        return f.read()
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
