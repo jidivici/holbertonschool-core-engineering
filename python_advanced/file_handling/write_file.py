bb5590b7d5fa054829640c21518ec8595ab8323e
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-07-09 14:38:27 +0200

    Add: websocker client

--- python_advanced/file_handling/write_file.py
+++ python_advanced/file_handling/write_file.py
@@ -5,6 +5,5 @@
 def write_file(filename="", text=""):
     """Writes text to the given file and returns
     the number of characters written."""
-
     with open(filename, mode="w", encoding="utf-8") as f:
         return f.write(text)
